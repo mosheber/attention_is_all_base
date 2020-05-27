6c6d4fec65e39331623db4e6f63b0652fa7d0b91
Your Name <you@example.com>
2020-05-27 09:10:27 +0300

    base repo

---
+++ attention_is_all_base/attention.py
@@ -0,0 +1,145 @@
+import tensorflow as tf
+import numpy as np
+
+def layer_norm(inputs, epsilon=1e-8):
+    mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+    normalized = (inputs - mean) / (tf.sqrt(variance + epsilon))
+
+    params_shape = inputs.get_shape()[-1:]
+    gamma = tf.get_variable('gamma', params_shape, tf.float32, tf.ones_initializer())
+    beta = tf.get_variable('beta', params_shape, tf.float32, tf.zeros_initializer())
+    
+    outputs = gamma * normalized + beta
+    return outputs
+
+def multihead_attn(queries, keys, q_masks, k_masks, future_binding, num_units, num_heads):
+    
+    T_q = tf.shape(queries)[1]                                      
+    T_k = tf.shape(keys)[1]                  
+
+    Q = tf.layers.dense(queries, num_units, name='Q')                              
+    K_V = tf.layers.dense(keys, 2*num_units, name='K_V')    
+    K, V = tf.split(K_V, 2, -1)        
+
+    Q_ = tf.concat(tf.split(Q, num_heads, axis=2), axis=0)                         
+    K_ = tf.concat(tf.split(K, num_heads, axis=2), axis=0)                    
+    V_ = tf.concat(tf.split(V, num_heads, axis=2), axis=0)                      
+
+    align = tf.matmul(Q_, tf.transpose(K_, [0,2,1]))                      
+    align = align / np.sqrt(K_.get_shape().as_list()[-1])                 
+
+    paddings = tf.fill(tf.shape(align), float('-inf'))                   
+
+    key_masks = k_masks                                                 
+    key_masks = tf.tile(key_masks, [num_heads, 1])                       
+    key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, T_q, 1])            
+    align = tf.where(tf.equal(key_masks, 0), paddings, align)       
+
+    if future_binding:
+        lower_tri = tf.ones([T_q, T_k])                                          
+        lower_tri = tf.linalg.LinearOperatorLowerTriangular(lower_tri).to_dense()  
+        masks = tf.tile(tf.expand_dims(lower_tri,0), [tf.shape(align)[0], 1, 1]) 
+        align = tf.where(tf.equal(masks, 0), paddings, align)                      
+    
+    align = tf.nn.softmax(align)                                            
+    query_masks = tf.to_float(q_masks)                                             
+    query_masks = tf.tile(query_masks, [num_heads, 1])                             
+    query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, T_k])            
+    align *= query_masks
+    
+    outputs = tf.matmul(align, V_)                                                 
+    outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=2)             
+    outputs += queries                                                             
+    outputs = layer_norm(outputs)                                                 
+    return outputs
+
+
+def pointwise_feedforward(inputs, hidden_units, activation=None):
+    outputs = tf.layers.dense(inputs, 4*hidden_units, activation=activation)
+    outputs = tf.layers.dense(outputs, hidden_units, activation=None)
+    outputs += inputs
+    outputs = layer_norm(outputs)
+    return outputs
+
+
+def learned_position_encoding(inputs, mask, embed_dim):
+    T = tf.shape(inputs)[1]
+    outputs = tf.range(tf.shape(inputs)[1])                # (T_q)
+    outputs = tf.expand_dims(outputs, 0)                   # (1, T_q)
+    outputs = tf.tile(outputs, [tf.shape(inputs)[0], 1])   # (N, T_q)
+    outputs = embed_seq(outputs, T, embed_dim, zero_pad=False, scale=False)
+    return tf.expand_dims(tf.to_float(mask), -1) * outputs
+
+
+def sinusoidal_position_encoding(inputs, mask, repr_dim):
+    T = tf.shape(inputs)[1]
+    pos = tf.reshape(tf.range(0.0, tf.to_float(T), dtype=tf.float32), [-1, 1])
+    i = np.arange(0, repr_dim, 2, np.float32)
+    denom = np.reshape(np.power(10000.0, i / repr_dim), [1, -1])
+    enc = tf.expand_dims(tf.concat([tf.sin(pos / denom), tf.cos(pos / denom)], 1), 0)
+    return tf.tile(enc, [tf.shape(inputs)[0], 1, 1]) * tf.expand_dims(tf.to_float(mask), -1)
+
+def label_smoothing(inputs, epsilon=0.1):
+    C = inputs.get_shape().as_list()[-1]
+    return ((1 - epsilon) * inputs) + (epsilon / C)
+
+class Attention:
+    def __init__(self, size_layer, embedded_size, learning_rate, size, output_size,
+                 num_blocks = 2,
+                 num_heads = 8, activation=tf.nn.relu,dense_layer_depth = 32,dense_layer_divide=2,class_weights=[0.5, 1.5],sequence_length=slices):
+        self.X = tf.placeholder(tf.float32, (None, sequence_length, size))
+        self.Y = tf.placeholder(tf.float32, (None, output_size))
+        self.activation = activation
+        self.dense_layer_depth = dense_layer_depth
+        self.dense_layer_divide=dense_layer_divide
+        self.class_weights=class_weights
+        self.sequence_length=sequence_length
+
+        encoder_embedded = tf.layers.dense(self.X, embedded_size)
+        encoder_embedded = tf.nn.dropout(encoder_embedded, keep_prob = 0.8)
+        x_mean = tf.reduce_mean(self.X, axis = 2)
+        en_masks = tf.sign(x_mean)
+        encoder_embedded += sinusoidal_position_encoding(self.X, en_masks, embedded_size)
+        
+        for i in range(num_blocks):
+            with tf.variable_scope('encoder_self_attn_%d'%i,reuse=tf.AUTO_REUSE):
+                encoder_embedded = multihead_attn(queries = encoder_embedded,
+                                             keys = encoder_embedded,
+                                             q_masks = en_masks,
+                                             k_masks = en_masks,
+                                             future_binding = False,
+                                             num_units = size_layer,
+                                             num_heads = num_heads)
+
+            with tf.variable_scope('encoder_feedforward_%d'%i,reuse=tf.AUTO_REUSE):
+                encoder_embedded = pointwise_feedforward(encoder_embedded,
+                                                    embedded_size,
+                                                    activation = tf.nn.relu)
+        dense_layer_current = encoder_embedded[:,-1] #.mean(axis=1)#
+        current_depth = size
+        while(current_depth >= self.dense_layer_depth):
+          print('current_depth: {} '.format(current_depth))
+          current_depth = int(np.round(current_depth / self.dense_layer_divide))
+          dense_layer_current = tf.layers.dense(dense_layer_current, current_depth ,activation=self.activation)          
+
+        self.logits = tf.layers.dense(dense_layer_current, output_size,activation=self.activation)
+        # self.cost = tf.reduce_mean(tf.square(self.Y - self.logits))
+        print('self.class_weights: {}'.format(self.class_weights))
+
+        class_weights = tf.constant([list(self.class_weights)])
+
+        # deduce weights for batch samples based on their true label
+        weights = tf.reduce_sum(class_weights * self.Y, axis=1)
+
+        # compute your (unweighted) softmax cross entropy loss
+        unweighted_losses = tf.nn.softmax_cross_entropy_with_logits(labels=self.Y,logits=self.logits)
+
+        # apply the weights, relying on broadcasting of the multiplication
+        weighted_losses = unweighted_losses * weights
+
+        # reduce the result to get your final loss
+        self.cost = tf.reduce_mean(weighted_losses)
+
+        self.optimizer = tf.train.AdamOptimizer(learning_rate).minimize(
+            self.cost
+        )
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(name='attention_is_all_base',
+      version='0.1',
+      description='attention_is_all_base',
+      author='huseinzol05',
+      author_email='huseinzol05',
+      packages=find_packages(),
+     )
